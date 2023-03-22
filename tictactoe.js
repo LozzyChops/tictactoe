1147ab3e9df887bf58366e30c710b10aba9d4404
LozzyChops <lephilim@gmail.com>
2023-03-22 14:01:55 -0500

    Add functionality for updating the display

--- tictactoe.js
+++ tictactoe.js
@@ -1,29 +1,34 @@
 const gameBoard = (function() {
     'use strict';
 
-    const board = [['X', 'X', 'X',], ['O', 'O', 'O'], ['X', 'X', 'X']];
+    const board = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
 
     const getBoard = () => board;
 
-    const labelSpace = (space, player) => {
-        board[space] = player;
-    }
-
-    return { getBoard, labelSpace };
+    return { getBoard };
 })();
 
 const displayController = (function() {
     'use strict';
 
-})();
+    const buttons = document.getElementsByClassName('boardSection');
+    const board = gameBoard.getBoard();
 
-const Player = (name) => {
-    let playerName = name;
-    let sign = 'X';
-    return {playerName, sign};
-};
+    const updateDisplay = () => {
+        for (let i = 0; i < buttons.length; i++) {
+            const button = buttons[i];
+            button.innerHTML = board[i];
+        }
+    }
+
+    return { updateDisplay };
+})();
 
 const gameController = (function() {
     'use strict';
 
+    return true;
 })();
+
+const Player = () => {
+};
