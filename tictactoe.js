3fca389e79a78d8819361ded2345f55459f29cf8
LozzyChops <lephilim@gmail.com>
2023-04-18 17:16:09 -0500

    Fix lack of win and tie game states

--- tictactoe.js
+++ tictactoe.js
@@ -1,34 +1,192 @@
-const gameBoard = (function() {
-    'use strict';
+'use strict';
 
-    const board = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
+const gameBoard = (function () {
+    let boardArray = new Array(9);
 
-    const getBoard = () => board;
+    const initBoardArray = function () {
+        boardArray = ["", "", "", "", "", "", "", "", ""];
+    };
 
-    return { getBoard };
+    const getBoardArray = () => {
+        return boardArray;
+    };
+
+    const updateBoardArray = (buttonId) => {
+        boardArray[buttonId] = gameController.getCurrentPlayer().getSymbol();
+    };
+    
+    return {
+        initBoardArray,
+        getBoardArray,
+        updateBoardArray,
+    };
 })();
 
-const displayController = (function() {
-    'use strict';
+const displayController = (function () {
+    const DOMButtons = document.getElementsByClassName("game-button");
 
-    const buttons = document.getElementsByClassName('boardSection');
-    const board = gameBoard.getBoard();
-
-    const updateDisplay = () => {
-        for (let i = 0; i < buttons.length; i++) {
-            const button = buttons[i];
-            button.innerHTML = board[i];
+    const initDOMButtons = function () {
+        for (let i = 0; i < DOMButtons.length; i++) {
+            DOMButtons[i].innerHTML = "";
+            DOMButtons[i].addEventListener("click", gameController.progressGame);
         }
-    }
+    };
 
-    return { updateDisplay };
+    const getDOMButtons = () => {
+        return DOMButtons;
+    };
+
+    const updateBoardDisplay = function (event) {
+        event.target.innerHTML = gameController.getCurrentPlayer().getSymbol();
+    };
+
+    return {
+        initDOMButtons,
+        getDOMButtons,
+        updateBoardDisplay,
+    };
 })();
 
-const gameController = (function() {
-    'use strict';
+const gameController = (function () {
+    let currentTurn;
+    let currentPlayer;
+    let playerOne;
+    let playerTwo;
+    let boardArray;
 
-    return true;
+    const Player = (name, symbol) => {
+        const getName = () => name;
+        const getSymbol = () => symbol;
+
+        return {
+            getName,
+            getSymbol,
+        };
+    };
+
+    const getCurrentTurn = () => {
+        return currentTurn;
+    };
+
+    const getCurrentPlayer = () => {
+        return currentPlayer;
+    };
+
+    const updateCurrentTurn = () => {
+        currentTurn++;
+
+        return currentTurn;
+    };
+
+    const updateCurrentPlayer = () => {
+        if (currentPlayer === playerOne) {
+            currentPlayer = playerTwo;
+            
+        } else {
+            currentPlayer = playerOne;
+        };
+
+        return currentPlayer;
+    };
+
+    const initGame = function () {
+        boardArray = gameBoard.initBoardArray();
+        playerOne = Player("Player 1", "X");
+        playerTwo = Player("Player 2", "O");
+        currentTurn = 1;
+        currentPlayer = playerOne;
+        displayController.initDOMButtons();
+
+        return {
+            currentTurn,
+            currentPlayer,
+            playerOne,
+            playerTwo,
+            boardArray,
+        };
+    };
+
+    const progressGame = function (event) {
+        if (isInvalidMove(event["target"]["id"])) return;
+        else {
+            gameBoard.updateBoardArray(event["target"]["id"]);
+            displayController.updateBoardDisplay(event);
+        };  
+
+        if (isWin()) {
+            console.log("someone won! ending game!")
+            endGame();
+        } else if (isTie()) {
+            console.log("tie! ending game!")
+            endGame();
+        } else {
+            updateCurrentTurn();
+            updateCurrentPlayer();
+        };
+    };
+
+    const isInvalidMove = (id) => (gameBoard.getBoardArray()[id] !== "");
+    
+    const isWin = () => {
+        switch (true) {
+            case (gameBoard.getBoardArray()[0] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[1] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[2] === currentPlayer.getSymbol()): {
+            return true;
+            };
+            case (gameBoard.getBoardArray()[3] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[4] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[5] === currentPlayer.getSymbol()): {
+            return true;
+            };
+            case (gameBoard.getBoardArray()[6] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[7] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[8] === currentPlayer.getSymbol()): {
+            return true;
+            };
+            case (gameBoard.getBoardArray()[0] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[3] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[6] === currentPlayer.getSymbol()): {
+            return true;
+            };
+            case (gameBoard.getBoardArray()[1] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[4] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[7] === currentPlayer.getSymbol()): {
+            return true;
+            };
+            case (gameBoard.getBoardArray()[2] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[5] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[8] === currentPlayer.getSymbol()): {
+            return true;
+            };
+            case (gameBoard.getBoardArray()[0] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[4] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[8] === currentPlayer.getSymbol()): {
+            return true;
+            };
+            case (gameBoard.getBoardArray()[2] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[4] === currentPlayer.getSymbol() &&
+                gameBoard.getBoardArray()[6] === currentPlayer.getSymbol()): {
+            return true;
+            };
+        };
+    };
+
+
+    const isTie = () => (getCurrentTurn() > 8);
+
+    const endGame = function () {
+        initGame();
+    };
+
+    let gameController = {
+        initGame,
+        progressGame,
+        getCurrentTurn,
+        getCurrentPlayer,
+    };
+
+    return gameController;
 })();
 
-const Player = () => {
-};
+gameController.initGame();
